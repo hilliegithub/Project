16a31d7e1f8ced51b2699a62f05da963c6235f6b
Hylton <goregehylton@yahoo.com>
2023-03-17 22:15:07 -0500

    Corrected File Naming Errors & Form Submits Ok

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 define('DB_DSN', 'mysql:host=localhost;dbname=serverside;charset=utf8');
 define('DB_USER', 'serveruser');
 define('DB_PASS', 'gorgonzola7!');
-
+define('BIKEFOLDER', 'bikeimages');
 try {
     // Try creating new PDO connection to MySQL.
     $db = new PDO(DB_DSN, DB_USER, DB_PASS);
--- processPost.php
+++ processPost.php
@@ -5,21 +5,43 @@ ini_set('display_errors', 1);
 
 require("connect.php");
 
-if (!$db) {
-    echo 'No Connection!';
-    die();
-}
-
+// if (!$db) {
+//     echo 'No Connection!';
+//     die();
+// }
+$inputError = false;
 
 if ($_POST) {
+    print_r($_POST);
     // Check that post is not empty and sanitize
-    $make = $_POST['make'];
-    $model = $_POST['model'];
-    $engine = $_POST['engine'];
-    $year = $_POST['year'];
-    $displacement = $_POST['displacement'];
-    $datecreated = new DateTime();
-    $imageURL = 'bikesimages/suzuki.jpg';
+
+    if (
+        !empty($_POST['make']) && !empty($_POST['model']) && !empty($_POST['engine']) &&
+        !empty($_POST['year']) && !empty($_POST['displacement']) && isset($_FILES['image']) && !($_FILES['image']['error'] > 0)
+    ) {
+
+        $image_filename = $_FILES['image']['name'];
+        $temporary_image_path = $_FILES['image']['tmp_name'];
+
+        $image_storageFld = get_image_original(basename($image_filename));
+
+        if (file_is_an_image($temporary_image_path, $image_storageFld)) {
+            move_uploaded_file($temporary_image_path, $image_storageFld);
+        }
+
+        //Sanitize inputs
+        $make = filter_input(INPUT_POST, 'make', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $model = filter_input(INPUT_POST, 'model', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $engine = filter_input(INPUT_POST, 'engine', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $year = filter_input(INPUT_POST, 'year', FILTER_VALIDATE_INT);
+        flagError($year);
+        $displacement = filter_input(INPUT_POST, 'displacement', FILTER_VALIDATE_FLOAT);
+        $datecreated = new DateTime();
+        $imageURL = $image_storageFld;
+    }
+
+
+    // MODIFY FOR USERID!!!!!!!
 
     $insertQuery = "INSERT INTO BikePost (make , model , year, engine, 
     displacement_ccm, image_url, userID, date_created) VALUES (:make, :model, :year, :engine, 
@@ -40,7 +62,46 @@ if ($_POST) {
     if (!$result) {
         die('Error executing query: ' . $statement->errorInfo()[2]);
     }
+    header("Location: index.php");
+    exit;
 }
 
+function get_image_original($original_name)
+{
+    $len = strlen(join(DIRECTORY_SEPARATOR, [dirname(__FILE__), BIKEFOLDER, $original_name]));
+
+    if ($len > 100) {
+        $shortName = join(DIRECTORY_SEPARATOR, [dirname(__FILE__), BIKEFOLDER, substr($original_name, 0, 10) . substr($original_name, -4)]);
+        return $shortName;
+    }
+
+    return join(DIRECTORY_SEPARATOR, [dirname(__FILE__), BIKEFOLDER, $original_name]);
+}
+
+function file_is_an_image($temporary_path, $new_path)
+{
+    $allowed_mime_types = ['image/jpeg', 'image/png'];
+    $allowed_file_extensions = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'];
+
+    $actual_file_extension = pathinfo($new_path, PATHINFO_EXTENSION);
+    $actual_mime_type = getimagesize($temporary_path)['mime'];
+
+    $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+    $mime_type_is_valid = in_array($actual_mime_type, $allowed_mime_types);
+
+    return $file_extension_is_valid && $mime_type_is_valid;
+}
+
+function subStringName($filename, $length)
+{
+    return substr($filename, 0, $length);
+}
+
+function flagError($value)
+{
+    if ($value === false || $value === null) {
+        $inputError = true;
+    }
+}
 
 ?>
