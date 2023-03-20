efff2a15f8ac08fe89685fdca03b73757283e6ec
Hylton <goregehylton@yahoo.com>
2023-03-19 20:45:25 -0500

    First User Regist. Password Hashing Done

--- processedRegister.php
+++ processedRegister.php
@@ -1,12 +1,64 @@
 <?php
-
+require("connect.php");
 $processingError = false;
 
 try {
     if ($_POST) {
-        print_r($_POST);
+        if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmpassword'])) {
+            print_r($_POST);
+
+            if ($_POST['password'] !== $_POST['confirmpassword']) {
+                throw new Exception('Passwords Do not match!');
+            }
+
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $password = $_POST["password"];
+
+            //Generate random salt for password
+            $salt = bin2hex(random_bytes(16));
+
+            $password_hashed = password_hash($password . $salt, PASSWORD_DEFAULT);
+
+            $query = "INSERT INTO user (username, email, password, salt, isAdmin) VALUES (:username, :email, :password_hashed, :salt, :isadmin)";
+
+            $all_bind_values = [
+                'username' => $username,
+                'email' => $email,
+                'password_hashed' => $password_hashed,
+                'salt' => $salt,
+                'isadmin' => 0
+            ];
+
+            $statement = $db->prepare($query);
+            $result = $statement->execute($all_bind_values);
+            if (!$result) {
+                // die('Error executing query: ' . $statement->errorInfo()[2]);
+                throw new Exception('Insert Failed');
+            }
+            throw new Exception('Testing');
+        }
     }
 } catch (Exception $e) {
     $processingError = true;
+    $errorMessage = $e->getMessage();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registering User</title>
+</head>
+
+<body>
+    <?php if($processingError): ?>
+    <?=$errorMessage?>
+    <?php endif ?>
+</body>
+
+</html>
--- register.php
+++ register.php
@@ -18,20 +18,34 @@ require("connect.php");
     <main>
         <h1>User Sign Up</h1>
         <div class="username-available">
-            Username is available. Please continue to fill out lovely form.
+            Username is available. Please continue to fill out the form.
         </div>
-        <div class="username-taken">
+        <div class="username-taken error">
             Sorry this username is taken. Please choose a different username.
         </div>
         <form action="processedRegister.php" method="post">
             <ul>
                 <li>
                     <label for="username">Username</label>
-                    <input type="text" id="username" placeholder="Username" name="username">
+                    <input type="text" id="username" placeholder="Username" name="username" required>
+                    <!-- <p class="error" id="invalid_username">* Invalid Username.</p> -->
+                </li>
+                <li>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" placeholder="user@email" name="email" required>
+                    <!-- <p class="error" id="invalid_username">* Invalid Username.</p> -->
                 </li>
                 <li>
                     <label for="password">Password</label>
-                    <input type="password" id="password" placeholder="Password" name="password">
+                    <input type="password" id="password" placeholder="Password" name="password" required>
+                </li>
+                <li>
+                    <label for="confirmpassword">Confirm Password</label>
+                    <input type="password" id="confirmpassword" placeholder="Confirm Password" name="confirmpassword"
+                        required>
+                </li>
+                <li>
+                    <p class="error" id="password_not_match">* Passwords do not match.</p>
                 </li>
                 <li>
                     <input id="submit" name="submit" type="submit" />
